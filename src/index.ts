ab065fa41378357cb638eeb1d5176c61aeb6fe01
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-10-07 10:22:53 -0300

    iniciando

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
-// import db from "./client/db";
+import db from "./client/db";
+import newUser from "./client/db";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 // Exercicio de CRUD
 // Utilizando as 5 funções encontradas em db, crie 5 endpoints para o recurso "usuario".
@@ -19,9 +21,24 @@ app.use(express.urlencoded({ extended: true }));
     }
 */
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
+ app.get("/users", async (req, res) => {
+   try{
+     const users = await db.findAll();
+     res.status(200).json(users);
+   }catch(error){
+      res.status(500).json({message: 'Usuários não localizados', error})
+   }
+ });
+
+ app.post("/create", async (req, res) => {
+  const {id, name, email, password} = req.body;
+  try{
+    const usuario = await db.create({id, name, email, password});
+    res.status(201).json(usuario);
+  }catch(error){
+     res.status(500).json({message: 'Erro ao criar usuário', error})
+  }
+});
 
 app.listen(port, () => {
   console.log(`Esse servidor está rodando em ${port}`);
