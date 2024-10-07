862a5c1ebf12ff96942308dd6574ca2b13e11d10
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-10-07 16:09:21 -0300

    busca por id

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./client/db";
-import newUser from "./client/db";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -21,24 +20,38 @@ app.use(express.urlencoded({ extended: true }));
     }
 */
 
- app.get("/users", async (req, res) => {
-   try{
-     const users = await db.findAll();
-     res.status(200).json(users);
-   }catch(error){
-      res.status(500).json({message: 'Usuários não localizados', error})
-   }
- });
-
+//Criar um usuário
  app.post("/create", async (req, res) => {
   const {id, name, email, password} = req.body;
   try{
-    const usuario = await db.create({id, name, email, password});
-    res.status(201).json(usuario);
+    const criarusuario = await db.create({id, name, email, password});
+    res.status(201).json(criarusuario);
   }catch(error){
      res.status(500).json({message: 'Erro ao criar usuário', error})
   }
 });
+
+//Buscar todos
+app.get("/users", async (req, res) => {
+  try{
+    const users = await db.findAll();
+    res.status(200).json(users);
+  }catch(error){
+     res.status(500).json({message: 'Usuários não localizados', error})
+  }
+});
+
+//Buscar por ID
+app.get("/users/:id", async (req, res) => {
+  const id = parseInt(req.params.id); //garante que o valor do parâmetro seja convertido para um número inteiro antes de ser usado para buscas no banco de dados.
+  try{
+    const usuario = await db.findById(id);
+    res.status(200).json(usuario);
+  }catch(error){
+     res.status(500).json({message: 'Usuário não localizado', error})
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Esse servidor está rodando em ${port}`);
